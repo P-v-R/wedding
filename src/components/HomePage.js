0798c6e6798b5a80416833a100728c4e2eda3e62
Perry von Rosenvinge <72633467+P-v-R@users.noreply.github.com>
2022-05-17 14:28:31 -0700

    feat: expand site remove image

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,16 @@
 import React from "react";
 import StickyBox from "react-sticky-box";
 import SideNav from './SideNav.js'
-import homeImage from '../assets/homeImage.jpg'
 import WhereAndWhen from "./WhereAndWhen.js";
 import BodyOne from "./BodyOne.js";
 import BodyTwo from "./BodyTwo.js";
+import LandingAnimation from "./LandingAnimation.js";
 
 function HomePage() {
   return (
     <div className="grid grid-cols-4 sticky fixed">
       <div className="md:col-span-3 col-span-4">
-        <img className="hidden md:flex object-fill" src={homeImage} alt="hotel logo"/>
+        <LandingAnimation/>
         <WhereAndWhen/>
         <BodyOne/>
         <BodyTwo/>
