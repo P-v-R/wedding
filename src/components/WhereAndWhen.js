b75cac240f23a0bdc9a68cd881cbd4cb8bbaee9f
Perry von Rosenvinge <72633467+P-v-R@users.noreply.github.com>
2022-05-17 18:05:39 -0700

    feat: build registry change landing svg

--- src/components/WhereAndWhen.js
+++ src/components/WhereAndWhen.js
@@ -7,14 +7,25 @@ import { Parallax } from "react-scroll-parallax";
 function WhereAndWhen() {
   return (
     <div>
-      <Parallax speed={10}>
-        <HotelLogo />
-        <div className="mx-auto text-base">
-          <div className="bg-base mt-12 pt-4 pb-20 z-20">
-            <div className="text-4xl text-darkGreen">October 2023</div>
+      <div className="hidden md:block">
+        <Parallax speed={10}>
+          <HotelLogo />
+          <div className="mx-auto text-base">
+            <div className="bg-base mt-12 pt-4 pb-20 z-20">
+              <div className="text-4xl text-darkGreen">October 2023</div>
+            </div>
           </div>
-        </div>
-      </Parallax>
+        </Parallax>
+      </div>
+      <div className="md:hidden">
+          <HotelLogo />
+          <div className="mx-auto text-base">
+            <div className="bg-base mt-12 pt-4 pb-20 z-20">
+              <div className="text-4xl text-darkGreen">October 2023</div>
+            </div>
+          </div>
+      </div>
+
     </div>
   );
 }
