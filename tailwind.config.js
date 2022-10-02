889b7be4bffef3c6ffff069fb156aa17c947c90e
Perry von Rosenvinge <72633467+P-v-R@users.noreply.github.com>
2022-10-02 08:35:10 -0700

    progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      white: '#ffffff',
+      white: "#ffffff",
       base: "#fffcec",
       darkGreen: "#719259",
       clay: "#DD8955",
@@ -11,14 +11,17 @@ module.exports = {
       pink: "#df7665",
       lightPink: "#f9b1a2",
       gold: "#e0c79d",
-      
+
       naturalWhite: "#d2cec5",
       freshAir: "#b0bcc8",
       olive: "#719259",
       sorbet: "#fff6be",
       papaya: "#df7665",
       coral: "#f9b1a2",
-      chartreuse: "#b3b355"
+      chartreuse: "#b3b355",
+    },
+    backgroundImage: {
+
     },
     extend: {},
   },
